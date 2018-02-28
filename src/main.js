1ef67c0a5f0394df6c97599c93853815a7069777
menxu <menxu_work@163.com>
2018-02-28 21:32:13 +0800

    vct init

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import Element from 'element-ui'
+import store from './store'
+import '@/permission' // permission control
+import './mock'
+
+Vue.config.productionTip = false
+
+Vue.use(Element)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
